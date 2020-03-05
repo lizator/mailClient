6546798eb3b49a745c295c7ecd5ef346464a6b0b
lizator <frederikkoefoeds@gmail.com>
2020-03-05 23:28:12 +0100

    Working mail system with pictures

--- Mail Client.py
+++ Mail Client.py
@@ -9,13 +9,13 @@ def sendmsg(msg, response=True):
         return recv
 
 
-subject = "Testing computer networks!"
-msg = "\r\n I love computer networks!"
+subject = "Email about completed assignment!"
+msg = "\r\n I love computer networks! \n and you do too!"
 endmsg = "\r\n.\r\n"
 TOMAIL = "smtpcontesting@outlook.com"
 FROMMAIL = "testing@dtu.dk"
-picName = "testPic.png"
-picPath = "./" + picName
+picName = "IkkeKaj.png"
+picPath = "" + picName
 
 
 hostName = "localhost"
@@ -32,7 +32,7 @@ if recv[:3] != '220':
     print('220 reply not received from server.')
     exit(-1)
 
-heloCommand = 'EHLO Alice\r\n' # EHLO for extended SMTP
+heloCommand = 'HELO Alice\r\n' # EHLO for extended SMTP
 
 clientSocket.send(heloCommand.encode())
 recv1 = clientSocket.recv(1024).decode()
@@ -42,21 +42,20 @@ print("msg after EHLO: " + recv1)
 if recv1[:3] != '250':
     print('250 reply not received from server.')
 
-
-username = "smtpcontesting@outlook.com"
+# Openning pic in read binary mode and encrypting it for departure
 
 pic = open(picPath, "rb")
 base64str = base64.b64encode(pic.read())
-
+print(base64str)
 # Send MAIL FROM command and print server response.
 
-mailFrom = "MAIL FROM:bhsi@dtu.dk\r\n"
+mailFrom = "MAIL FROM:" + FROMMAIL + "\r\n"
 recv2 = "Response after MAIL FROM: " + sendmsg(mailFrom)
 print(recv2)
 
 # Send RCPT TO command and print server response.
 
-rcptTo = "RCPT TO: " + username + "\r\n"
+rcptTo = "RCPT TO: " + TOMAIL + "\r\n"
 recv3 = "Response after RCPT TO: " + sendmsg(rcptTo)
 print (recv3)
 
@@ -64,30 +63,26 @@ print (recv3)
 # Send DATA command and print server response.
 data = "DATA\r\n"
 sendmsg(data, False)
-#recv4 = "Response after 1st DATA: " + sendmsg(data)
-#print(recv4)
-
 
 # Send message data.
 
 sendmsg("Subject:" + subject + "\r\n", False)
 
 # picture
-sendmsg("MIME-Version: 1.0", False)
-sendmsg("Content-Type:multipart/mixed;boundary=\"separator\"", False)
-sendmsg("--separator", False)
-sendmsg("Content-Type:application/octet-stream;name=\"" + picName + "\"", False)
-sendmsg("Content-Type:application/octet-stream;name=\"" + picName + "\"", False)
-sendmsg("Content-Transfer-Encoding:base64", False)
-sendmsg("Content-Disposition:attachment;filename=\"" + picName + "\"", False)
-sendmsg("", False)
-sendmsg(base64str, False)
-sendmsg("", False)
-sendmsg("", False)
-sendmsg("--separator", False)
-sendmsg(" ", False)
-sendmsg(" ", False)
 
+print(sendmsg("MIME-Version: 1.0" + "\r\n"))
+sendmsg("Content-Type:multipart/mixed;boundary=\"separator\"" + "\r\n", False)
+sendmsg("--separator" + "\r\n", False)
+sendmsg("Content-Type:application/octet-stream;name=\"" + picName + "\"" + "\r\n", False)
+sendmsg("Content-Transfer-Encoding:base64" + "\r\n", False)
+sendmsg("Content-Disposition:attachment;filename=\"" + picName + "\"" + "\r\n", False)
+sendmsg("" + "\r\n", False)
+sendmsg(base64str + "\r\n", False)
+sendmsg("" + "\r\n", False)
+sendmsg("" + "\r\n", False)
+sendmsg("--separator" + "\r\n", False)
+sendmsg(" " + "\r\n", False)
+sendmsg(" " + "\r\n", False)
 
 # msg
 msgdata = msg + "\r\n"
@@ -96,6 +91,7 @@ sendmsg(msgdata, False)
 
 # Message ends with a single period.
 
+sendmsg("" + "\r\n", False)
 recv4 = "Response after end DATA: " + sendmsg(endmsg)
 print(recv4)
 
